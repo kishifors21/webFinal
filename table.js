33048d4cf8730b2ff94feef21b758e747e40d77e
Jing <63216272+kishifors21@users.noreply.github.com>
2022-01-11 01:36:04 +0800

    update

--- table.js
+++ table.js
@@ -1,5 +1,15 @@
+var uri = "http://140.128.102.242:8080";
+
+function renew(){
+  genTable = document.getElementsByClassName('genTable');
+  genTable.remove;
+  while(genTable.length > 0){
+    genTable[0].parentNode.removeChild(genTable[0]);
+}
+  drawTable();
+}
 async function drawTable() {
-  var jsondata = await fetch('http://140.128.102.242:8080/getdata')
+  var jsondata = await fetch(uri+'/getdata')
     .then(function(response) {
       return response.json();
     })
@@ -10,9 +20,10 @@ async function drawTable() {
   var tr, td;
   tbody = document.getElementById('tbody');
 
-  for (var i = 0; i < jsondata.length; i++) // loop through data source
+  for (var i = jsondata.length-1; i >=0 ; i--) // loop through data source
   {
     tr = tbody.insertRow(tbody.rows.length);
+    tr.classList.add('genTable');
     td = tr.insertCell(tr.cells.length);
     td.innerHTML = jsondata[i]['date'];
     td = tr.insertCell(tr.cells.length);
@@ -29,28 +40,31 @@ async function newData() {
   var tr, td;
   var data = document.getElementsByClassName('newData');
   console.log(data)
-  console.log(data[1])
 
   // for (var i = 0; i < data.length; i++) // loop through data source
   // {
 
   // }
-  let vody = {
-    date: data[0].value,
-    in: data[1].value,
-    out: data[2].value,
-    amount: data[3].value,
-  }
-  fetch("http://140.128.102.242:8080/newdata", {
-      method: "GET",
+
+  var body = {
+    "date": new Date().toISOString().slice(0, 19).replace('T', ' '),
+    "in": data[0].value,
+    "out": data[1].value,
+    "amount": data[0].value-data[1].value,
+  };
+  console.log(body);
+  await fetch(uri+"/newdata", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
       },
-      body: body,
+      body: JSON.stringify(body),
     })
     .then((response) => response.json())
     .then((json) => console.log(json));
+  renew();
+
 }
 $(document).ready(function() {
 
